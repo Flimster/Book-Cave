6f8c211251e6da24ab72d3a053387d60ef89199f
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-05-02 13:26:24 +0000

    Added the files to start the project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Book_Cave.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
