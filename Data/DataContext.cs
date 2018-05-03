99381e2ff3422911d3d8a91dd1fe4f3f00ea8bd7
tyler <tylereliasj@gmail.com>
2018-05-03 08:36:07 +0000

    Connection string to db

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Book_cave.Data
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder
+                .UseSqlServer(
+                    "Server=tcp:verklegt2.database.windows.net,1433;Initial Catalog=VLN2_2018_H01;Persist Security Info=False;User ID=VLN2_2018_H01_usr;Password=cut3Rose64;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }        
+    }
+}
