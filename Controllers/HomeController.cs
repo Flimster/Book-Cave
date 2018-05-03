aec1708c5e025be5a6ab3299a27c76976bfd381e
hellpro <brynjarorng@gmail.com>
2018-05-03 13:03:11 +0000

    Fixed authentication. Fully working create and login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Book_Cave.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Book_Cave.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
