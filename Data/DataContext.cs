ffcf810714e523b8e881a009df766fc29f3b438c
tyler <tylereliasj@gmail.com>
2018-05-04 10:25:24 +0000

    Fixed Data errors

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Book_Cave.Data.DatabaseExtraction;
+using BookCave.Data.EntityModels;
 namespace BookCave.Data
 {
     public class DataContext : DbContext
@@ -7,29 +7,14 @@ namespace BookCave.Data
         public DbSet<Address> Address {get; set;}
         public DbSet<Author> Author {get; set;}
         public DbSet<Book> Book {get; set;}
-        public DbSet<BookAuthor> BookAuthor {get; set;}
-        public DbSet<BookGenre> BookGenre {get; set;}
-        public DbSet<BookLanguage> BookLanguage {get; set;}
-        public DbSet<BookReview> BookReview {get; set;}
         public DbSet<CardDetails> CardDetails {get; set;}
         public DbSet<Country> Country {get; set;}
         public DbSet<Feedback> Feedback {get; set;}
         public DbSet<Format> Format {get; set;}
         public DbSet<Genre> Genre {get; set;}
         public DbSet<Language> Language {get; set;}
-        public DbSet<OrderBooklist> OrderBooklist {get; set;}
-        public DbSet<Orders> Orders {get; set;}
-        public DbSet<OwnedBook> OwnedBook {get; set;}
         public DbSet<Publisher> Publisher {get; set;}
         public DbSet<Review> Review {get; set;}
-        public DbSet<UserAccount> UserAccount {get; set;}
-        public DbSet<UserBilling> UserBilling {get; set;}
-        public DbSet<UserCard> UserCard {get; set;}
-        public DbSet<UserPayment> UserPayment {get; set;}
-        public DbSet<UserReadBook> UserReadBook {get; set;}
-        public DbSet<UserReview> UserReview {get; set;}
-        public DbSet<UserShipping> UserShipping {get; set;}
-        public DbSet<Wishlist> Wishlist {get; set;}
 
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
