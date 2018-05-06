af3aeec47538ffc3326c3e6de5f9480b8bd547df
Brynjar <brynjarorng@gmail.com>
2018-05-06 00:42:32 +0000

    Working role changer. Name needs to be set correctly when account is crated in the service

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,15 +2,14 @@ using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading.Tasks;
 using BookCave.Data.EntityModels;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Book_Cave.Controllers
 {
-    [Authorize(Roles = "Administrator")]
-    public class AdminController
+    //[Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
     {
 
         private readonly UserManager<AspNetUsers> _userManager;
@@ -19,7 +18,13 @@ namespace Book_Cave.Controllers
         {
             _userManager = userManager;
         }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
         
+        [HttpPost]
         //Function that changes any claim
         public async Task ChangeClaim(string email, string newClaim)
         {
