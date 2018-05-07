77721a25ead170685b588fcfccc88bc928071590
Brynjar <brynjarorng@gmail.com>
2018-05-07 19:42:18 +0000

    small bug fixes in

--- FakeDatabase.cs
+++ FakeDatabase.cs
@@ -8,9 +8,9 @@ namespace BookCave
     {
 
         //public List<string> aa = new List<string> { "Abc", "Def"};
-/*
+
         public static List<BookViewModel> Books = new List<BookViewModel>()
-        {
+        {/*
             new BookViewModel { Id = 1, Title = "Harry Potter and the chamber of secrets", Authors = new List<Author> {"J.K Rowling"}, Genre = "Fantasy", Image = "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4088/9781408855669.jpg", Price = 9.99 }, 
             new BookViewModel { Id = 2, Title = "Harry Potter and the goblet of fire", Authors = new List<string> {"J.K Rowling"}, Genre = "Fantasy", Image = "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4088/9781408855683.jpg", Price = 12.99},
             new BookViewModel { Id = 3, Title = "Head first C#", Authors = new List<string> {"Abcd"}, Genre = "Computer Science", Image = "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4493/9781449343507.jpg", Price = 13.99},
@@ -20,7 +20,7 @@ namespace BookCave
             new BookViewModel { Id = 7, Title = "Animal Farm", Authors = new List<string> {"George Orwell"}, Genre = "Fiction", Image = "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1410/9780141036137.jpg", Price = 7.99 },
             new BookViewModel { Id = 8, Title = "Lord of the Flies", Authors = new List<string> {"William Golding"}, Genre = "Fiction", Image = "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/3995/9780399501487.jpg", Price = 12.99 },
             new BookViewModel { Id = 9, Title = "The Catcher in the Rye", Authors = new List<string> {"J.D. Salinger"}, Genre = "Fiction", Image = "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/3167/9780316769488.jpg", Price = 19.99 }
-        };
-        */
+        */};
+        
     }
 }
